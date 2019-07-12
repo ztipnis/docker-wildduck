7a0393991e98818f8666abe61dd70d44785ac029
Zachary Tipnis <tipniz@rpi.edu>
2019-07-12 09:27:43 -0400

    path fix

--- install/config.sh
+++ install/config.sh
@@ -1,6 +1,5 @@
 echo "Configuring"
 echo $PATH
-PATH=/bin:/usr/bin:/sbin:/usr/sbin:$PATH:/opt/.npm-global/bin
 set +e
 addgroup -S wildduck
 adduser -S -H -D wildduck -G wildduck
--- install/run.sh
+++ install/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+for x in ("/bin" "/sbin" "/usr/sbin" "/usr/bin" "/opt/.npm-global/bin"); do
+  case ":$PATH:" in
+    *":$x:"*) :;; # already there
+    *) PATH="$PATH:$x";;
+  esac
+done
 CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
 	touch $CONTAINER_ALREADY_STARTED
